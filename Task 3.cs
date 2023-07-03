713c31be76f62248ecc04d02c3ed682bc537ada5
unknown <narminna@code.edu.az>
2023-07-03 18:09:46 +0400

    Task 3

---
+++ Task 3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    public class MathHelper
+    {
+        public int PrintValue(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int PrintValue(bool value)
+        {
+            if (value)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void PrintValue(string str1, string str2)
+        {
+            string res = str1 + str2;
+            Console.WriteLine(res);
+        }
+
+        public int PrintValue(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper mathHelper = new MathHelper();
+            int sum = mathHelper.PrintValue(2, 3);
+            Console.WriteLine(sum);
+
+            int boolResult = mathHelper.PrintValue(true);
+            Console.WriteLine(boolResult);
+
+            mathHelper.PrintValue("Get", "Away");
+
+            int multiplication = mathHelper.PrintValue(2, 3, 4);
+            Console.WriteLine(multiplication);
+        }
+    }
+}
